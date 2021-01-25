857736c9cf2d4ee47b84dcb628163fd8ce720447
inobu <inohkunobuya@gmail.com>
2021-01-26 00:16:16 +0900

    take

--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -1,10 +1,19 @@
 package laziness
 
+import laziness.Stream._
+
 trait Stream[+A] {
   def toList: List[A] = this match {
     case Cons(h,t) => h() :: t().toList
     case _ => List()
   }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if(n > 1) => cons(h(), t().take(n - 1))
+    case Cons(h, _)  =>cons(h(), empty)
+    case _ => empty
+  }
+
 }
 
 case object Empty extends Stream[Nothing]
