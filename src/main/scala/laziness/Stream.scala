564f9f0ead2fd648491465c84f86fa0da5d744bb
inobu <inohkunobuya@gmail.com>
2021-03-02 21:00:09 +0900

    fibs

--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -80,6 +80,13 @@ trait Stream[+A] {
   def from(n: Int): Stream[Int] = {
     cons(n, from(n - 1))
   }
+
+  def fibs(): Stream[Int] = {
+    def go(f0: Int, f1: Int): Stream[Int] = {
+      cons(f0, go(f1, f0 + f1))
+    }
+    go(0, 1)
+  }
 }
 
 case object Empty extends Stream[Nothing]
